6806414797a6b4a732a672628ecc2311f8938c8e
JulioCesar1988 <julio.unlp2010@gmail.com>
2023-08-15 06:02:35 -0300

    file con temas  estudiar

---
+++ Parcial/src/parcialJunio2022/test.java
@@ -0,0 +1,11 @@
+package parcialJunio2022;
+
+public class test {
+
+
+    public static void main(String[] args) {
+        System.out.println("Prueba de Junio");
+    }
+
+
+}
---
+++ Parcial/src/parcialNoviembre2021/test.java
@@ -0,0 +1,8 @@
+package parcialNoviembre2021;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Prueba de Noviembre");
+    }
+}
